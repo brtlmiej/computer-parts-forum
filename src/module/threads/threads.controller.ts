584225f5e5c91813c0839b17ed744f83f8730bd1
Bartłomiej Ciołek <b.ciolek@gbxsoft.com>
2022-01-24 20:09:27 +0100

    feat: threads create

--- src/module/threads/threads.controller.ts
+++ src/module/threads/threads.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post, Redirect, Render, Res } from '@nestjs/common';
 import { ThreadsRepository } from './threads.repository';
+import { ThreadDto } from './dto/thread.dto';
+import { Thread } from './thread.entity';
+import { CommentsRepository } from '../comments/comments.repository';
+import { Response } from 'express';
 
 @Controller('threads')
 export class ThreadsController {
-  constructor(private readonly threadsRepository: ThreadsRepository) {}
+  constructor(
+    private readonly threadsRepository: ThreadsRepository,
+    private readonly commentsRepository: CommentsRepository,
+  ) {}
 
   @Get()
   @Render('threads/index')
@@ -14,5 +21,70 @@ export class ThreadsController {
         order: { createdAt: 'DESC' }
       });
     return { threads };
+  }
+
+  @Get('/:id')
+  @Render('threads/show')
+  async show(@Param('id') id: number) {
+    const thread = await this.threadsRepository.findOne(id);
+    if (!thread) {
+      throw new NotFoundException()
+    }
+    return { thread };
+  }
+
+  @Get('create')
+  @Render('threads/create')
+  async create() {
+    return {};
+  }
+
+  @Post()
+  @Redirect('/threads')
+  async store(@Body() body: ThreadDto) {
+    const thread = new Thread();
+    thread.title = body.title;
+    thread.description = body.description;
+    await this.threadsRepository.save(thread);
+  }
+
+  @Get('/:id/edit')
+  @Render('threads/edit')
+  async edit(@Param('id') id: number) {
+    const thread = await this.threadsRepository.findOne(id);
+    if (!thread) {
+      throw new NotFoundException()
+    }
+    return { thread };
+  }
+
+  @Post('/:id')
+  async update(
+    @Param('id') id: number,
+    @Body() body: ThreadDto,
+    @Res() response: Response,
+  ) {
+    const thread = await this.threadsRepository.findOne(id);
+    if (!thread) {
+      throw new NotFoundException()
+    }
+    thread.title = body.title;
+    thread.description = body.description;
+    await this.threadsRepository.save(thread);
+    await response.redirect('/threads/' + thread.id + '/edit')
+  }
+
+  @Post(':id/delete')
+  @Redirect('/')
+  async delete(
+    @Param('id') id: number,
+  ) {
+    const thread = await this.threadsRepository.findOne(id);
+    if (!thread) {
+      throw new NotFoundException()
+    }
+    const comments = await thread.comments;
+    await this.commentsRepository.remove(comments);
+    await this.threadsRepository.remove(thread);
   }
 }
--- src/module/threads/threads.module.ts
+++ src/module/threads/threads.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ThreadsController } from './threads.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ThreadsRepository } from './threads.repository';
+import { CommentsRepository } from '../comments/comments.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ThreadsRepository])],
+  imports: [TypeOrmModule.forFeature([
+    ThreadsRepository,
+    CommentsRepository
+  ])],
   controllers: [ThreadsController]
 })
 export class ThreadsModule {}

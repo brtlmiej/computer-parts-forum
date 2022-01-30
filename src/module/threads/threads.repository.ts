648da181108db8bef6198dc1217d36571486100c
Bartłomiej Ciołek <b.ciolek@gbxsoft.com>
2022-01-30 21:49:46 +0100

    csrf protection

--- src/module/threads/threads.repository.ts
+++ src/module/threads/threads.repository.ts
@@ -3,4 +3,11 @@ import { Thread } from './thread.entity';
 import { EntityRepository } from 'typeorm/index';
 
 @EntityRepository(Thread)
-export class ThreadsRepository extends BaseRepository<Thread> {}
+export class ThreadsRepository extends BaseRepository<Thread> {
+  async findOneByTitle(name: string) {
+    return await this.createQueryBuilder('threads')
+      .where('threads.name = :name')
+      .setParameters({ name: name })
+      .getOne();
+  }
+}
